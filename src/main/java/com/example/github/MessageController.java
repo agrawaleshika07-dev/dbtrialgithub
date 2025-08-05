bc525f0c785d2693c89f71d165f009ad84e3f1fa
agrawaleshika07-dev <agrawal.eshika07@gmail.com>
2025-08-05 23:31:37 +0530

    Initial commit from ServiceNow

---
+++ src/main/java/com/example/github/MessageController.java
@@ -0,0 +1,25 @@
+package com.example.github;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class MessageController {
+
+    private final MessageService messageService;
+
+    public MessageController(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    @PostMapping("/messages")
+    public void addMessage(@RequestBody String content) {
+        messageService.saveMessage(content);
+    }
+
+    @GetMapping("/messages")
+    public List<String> getAllMessages() {
+        return messageService.getAllMessages();
+    }
+}
